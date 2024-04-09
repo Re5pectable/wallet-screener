733d55053fe0ef7ced0319bd508f8b050fe0059b
Басистов Глеб <basistovga@cash-u.com>
2024-04-09 11:40:57 +0300

    fix

--- src/adapters/db/models.py
+++ src/adapters/db/models.py
@@ -5,8 +5,9 @@ from sqlalchemy.sql import func
 
 Base = declarative_base()
 
+
 class NetworkOrm(Base):
-    __tablename__ = 'networks'
+    __tablename__ = "networks"
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     created_at = sa.Column(sa.DateTime(), default=func.now())
     title = sa.Column(sa.String(32))
@@ -14,26 +15,26 @@ class NetworkOrm(Base):
 
 
 class PairLPOrm(Base):
-    __tablename__ = 'pair_lps'
+    __tablename__ = "pair_lps"
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     created_at = sa.Column(sa.DateTime(), default=func.now())
     contract = sa.Column(sa.String(64), index=True)
     type = sa.Column(sa.String(32))
-    
+
 
 class SwapOrm(Base):
-    __tablename__ = 'trades'
+    __tablename__ = "trades"
     id = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
     created_at = sa.Column(sa.DateTime(), default=func.now(), index=True)
     txn_hash = sa.Column(sa.String(256))
     wallet = sa.Column(sa.String(128), index=True)
-    pair_lp_id = sa.Column(sa.Integer, sa.ForeignKey('pair_lps.id'), index=True)
+    pair_lp_id = sa.Column(sa.Integer, sa.ForeignKey("pair_lps.id"), index=True)
     pool_token0_delta = sa.Column(postgresql.NUMERIC(64, 0))
     pool_token1_delta = sa.Column(postgresql.NUMERIC(64, 0))
-    
+
 
 class ErrorOrm(Base):
-    __tablename__ = 'errors'
+    __tablename__ = "errors"
     id = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
     created_at = sa.Column(sa.DateTime(), default=func.now(), index=True)
     action = sa.Column(sa.String(32))
